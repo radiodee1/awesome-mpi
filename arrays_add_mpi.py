63f59558c3456224dc8b730e76c926b9fd2e8995
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-09 14:01:56 +0000

    first commit

---
+++ arrays_add_mpi.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI
+import numpy
+import math
+
+com = MPI.COMM_WORLD
+rank = com.Get_rank()
+end = com.Get_size()
+
+
+
+def add(size=10):
+	starttime = MPI.Wtime()
+	
+	data = numpy.zeros(shape=(size*3), dtype=numpy.float64)
+
+	#print data, 'bcast'
+	for i in range(size):
+		data[i] = int(i )
+		data[size + i] = int(i )
+		data[(size * 2) + i] = int(0)
+
+	com.bcast(data, root=0)
+	
+
+		
+	# on cores...
+	if rank != 0 :
+	
+		k = int (math.ceil(size/float(end - 1)))
+	
+		for i in range(0, size, 1) :
+
+			if (i >= (rank - 1) * k ) and (i  < (rank ) * k) : #k  +(( end)*(rank )) ) +(end ) :
+				data[(size * 2) + i] = data[i] + data[size + i]	
+				#com.Send(data[(size * 2) + i], dest=0, tag=i)
+				#print data[(size * 2) + i], "node", rank
+	
+	if rank == 0 :
+		endtime = MPI.Wtime()
+		print size, endtime - starttime
+	
+
+
+if True:
+	print "arrays_add_mpi"
+	add(100)
+	add(1000)
+	add(10000)
+	add(100000)
+	add(1000000)
---
+++ arrays_cpu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import time
+
+def add(size=10):
+
+    starttime = time.clock()
+	
+    a = tuple([float(i) for i in range(size)])
+    b = tuple([float(j) for j in range(size)])
+    c = [None for i in range(size)]
+    for i in range(size):
+        c[i] = a[i]+b[i]
+ 
+    #print "a", a
+    #print "b", b
+    #print "c", c[:1000]
+    endtime = time.clock()
+    print size, endtime - starttime
+ 
+#add(1000)
+#add(10000)
+#add(10000000)
