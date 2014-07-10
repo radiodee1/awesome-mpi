7fedc7880c4085669db5e33796ea8cd45435dd6b
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-10 20:22:42 +0000

    instructions for building python 2.7.8

--- system-scripts/su_yum_install.sh
+++ system-scripts/su_yum_install.sh
@@ -1,3 +1,5 @@
+yum -y update
+
 #yum install -y nfs-utils nfs-utils-lib svn
 
 #yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc
