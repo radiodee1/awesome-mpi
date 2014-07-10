b6a9a00eae1b42a529af7b48b0bfe2c312daf085
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-10 19:07:06 +0000

    later commit

--- trunk/system-scripts/su_yum_install.sh
+++ trunk/system-scripts/su_yum_install.sh
@@ -1,6 +1,6 @@
 #yum install -y nfs-utils nfs-utils-lib 
 
-#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
+#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc
 
 #wget http://python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
 #wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz --no-check-certificate
