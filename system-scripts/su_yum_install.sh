b3c49b7672f977ef25450bb18a772979f393bc58
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-10 20:19:32 +0000

    later commit

--- system-scripts/su_yum_install.sh
+++ system-scripts/su_yum_install.sh
@@ -1,11 +1,10 @@
-#yum install -y nfs-utils nfs-utils-lib 
+#yum install -y nfs-utils nfs-utils-lib svn
 
 #yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc
 
-#wget http://python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
 #wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz --no-check-certificate
-#tar xf Python-2.7.6.tar.xz
-#cd Python-2.7.6
+#tar xf Python-2.7.8.tgz
+#cd Python-2.7.8
 #./configure --prefix=/usr/local --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
 #make && make altinstall
  
