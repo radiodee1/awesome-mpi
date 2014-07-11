5e9e49d58a7d32d94a2dc64380a2a2633c61650a
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-11 14:15:42 +0000

    mpi4py instructions

--- trunk/system-scripts/su_yum_install.sh
+++ trunk/system-scripts/su_yum_install.sh
@@ -5,6 +5,6 @@ yum -y update
 #yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc
 
 # python info here??
-#yum -y install python python-devel python-lib
+#yum -y install python python-devel python-lib numpy
  
 #yum -y install mpich2 
