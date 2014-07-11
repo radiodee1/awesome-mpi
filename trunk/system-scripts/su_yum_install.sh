ccc5d44603b14d28982a41fefebafee2560c0bd7
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-11 16:28:01 +0000

    mpich2 examples instructions

--- trunk/system-scripts/su_yum_install.sh
+++ trunk/system-scripts/su_yum_install.sh
@@ -2,7 +2,7 @@ yum -y update
 
 #yum install -y nfs-utils nfs-utils-lib svn 
 
-#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc gcc-c++ keychain
+#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc gcc-c++ keyutils keyutils-libs
 
 # python info here??
 #yum -y install python python-devel python-lib numpy
