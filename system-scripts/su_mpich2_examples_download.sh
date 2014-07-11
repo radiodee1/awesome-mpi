12857a00974fcbafd75c4c6cb541e7cf4c8e3ad9
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-11 16:16:48 +0000

    mpich2 examples instructions

--- system-scripts/su_mpich2_examples_download.sh
+++ system-scripts/su_mpich2_examples_download.sh
@@ -1,2 +1,6 @@
 
 wget http://www.mcs.anl.gov/research/projects/mpich2staging/goodell/downloads/tarballs/1.2.1/mpich2-1.2.1.tar.gz
+
+#this is downloaded so that we can build it and try out some of the example programs (which I couldn't find in)
+#the centos repository. NEVER execute 'make install'. Only execute './configure' and 'make'. Then you
+#can find the test programs in the 'example/' folder.
--- system-scripts/su_yum_install.sh
+++ system-scripts/su_yum_install.sh
@@ -1,10 +1,13 @@
 yum -y update
 
-#yum install -y nfs-utils nfs-utils-lib svn
+#yum install -y nfs-utils nfs-utils-lib svn 
 
-#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc
+#yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel compat-gcc-34-g77 gcc gcc-c++ keychain
 
 # python info here??
 #yum -y install python python-devel python-lib numpy
  
 #yum -y install mpich2 
+
+# openssh stuff here??
+#yum -y install openssh-server openssh openssh-client
