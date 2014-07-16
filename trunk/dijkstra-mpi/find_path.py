47c59e56bd78d123b04077c55e72173ba9183b34
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-16 16:24:07 +0000

    make dijkstra folder

--- trunk/dijkstra-mpi/find_path.py
+++ trunk/dijkstra-mpi/find_path.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 from mpi4py import MPI
 import array_setup as mp
@@ -12,7 +12,7 @@ rank = com.Get_rank()
 dim = com.Get_size()
 
 if (dim != 100 and rank == 0) :
-	print "size must be 10x10, or 100!!"
+	print ('size must be 10x10, or 100!!')
 #if dim != 100 :
 #	sys.exit()
 	
