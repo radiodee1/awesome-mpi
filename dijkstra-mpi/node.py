55c250a3da625c798153503f2b5761ed89bd0d35
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-16 19:17:25 +0000

    make dijkstra folder

--- dijkstra-mpi/node.py
+++ dijkstra-mpi/node.py
@@ -23,65 +23,69 @@ def find() :
 	#if True:
 		#visited = com.alltoall(mp.visited)
 		#mp.visited[rank] = visited
-		mp.visited = com.allgather(mp.visited[rank] )
 		
-		print "reduce",mp.visited
+		mp.visited = com.allgather(mp.visited[rank] )
+		#visited = com.allreduce(mp.visited[rank], op=MPI.MAX)
+		print "reduce", mp.visited
 		
 		if mp.visited[rank] == mp.FREE and \
-				(mp.main[rank] != mp.WALL or mp.main[rank] == mp.START ) \
-				and near_visited() :
-			if get_y(rank) == get_y(rank + 1) and rank + 1 < dim :
+				(mp.main[rank] != mp.WALL or mp.main[rank] == mp.START ): 
+			if get_y(rank) == get_y(rank + 1) and rank + 1 < dim and near_visited() :
 				check(rank + 1)
-			if get_y(rank) == get_y(rank - 1) and rank - 1 >= 0 :
+			if get_y(rank) == get_y(rank - 1) and rank - 1 >= 0 and near_visited() :
 				check(rank - 1)
-			if rank + 10 < dim :
+			if rank + 10 < dim and near_visited() :
 				check(rank + 10) 
-			if rank - 10 >= 0 :
+			if rank - 10 >= 0 and near_visited() :
 				check(rank - 10)
-			mp.visited[rank] = mp.VISITED
+			#mp.visited[rank] = mp.VISITED
+			print 'here (didn\'t fire check)'
 			
 		if mp.visited[rank] == mp.VISITED and mp.main[rank] == mp.END :
 			localflag = 1
 			print "end found!!" , rank
 		else:
 			localflag = 0
+			print 'not yet'
 			
 		flag = com.reduce(localflag, op=MPI.SUM)
 		if (rank == 0) :
 			print "end flag" , flag
-			
-
+		print 'after flag reduce'
 		#dist = numpy.array(mp.dist, dtype=numpy.long)
-
-
 		#mp.dist = com.Bcast([dist, MPI.INT], root=rank)
 
-			
+	'''		
 	#print path backwards.
 	#com.barrier()
 	if (rank == 0):# or True:
 		i = 0
-		found =com.reduce( mp.prev[(mp.endy * 10) + mp.endx], op=MPI.MAX)
+		found =com.allreduce( mp.prev[(mp.endy * 10) + mp.endx], op=MPI.MAX)
 		print rank,"found start", found, 'test', test
 		while (found != 0) and i < 100 :
-			found = mp.prev[found]
-			#found = com.reduce(mp.prev[found], op=MPI.MAX);
+			#found = mp.prev[found]
+			found = com.allreduce(mp.prev[found], op=MPI.MAX);
 			i += 1
 			print found," ",
-	
+	'''
 	
 def near_visited() :
+	
 	if get_y(rank) == get_y(rank + 1) and rank + 1 < dim :
 		if mp.visited[rank + 1] == mp.VISITED:
+			mp.visited[rank] = mp.VISITED
 			return True
 	if get_y(rank) == get_y(rank - 1) and rank - 1 >= 0 :
 		if mp.visited[rank - 1] == mp.VISITED:
+			mp.visited[rank] = mp.VISITED
 			return True
 	if rank + 10 < dim:
 		if mp.visited[rank + 10] == mp.VISITED:
+			mp.visited[rank] = mp.VISITED
 			return True
 	if rank - 10 >= 0:
 		if mp.visited[rank -10] == mp.VISITED:
+			mp.visited[rank] = mp.VISITED			
 			return True
 	if rank == get_rank(mp.startx, mp.starty) :
 		return True
@@ -89,14 +93,12 @@ def near_visited() :
 		
 def check(test) :
 	#all distances are '1' !!
-	#test is unvisited node!!
-	#if mp.visited[test] != mp.VISITED :
-	if True:
-		#dist = mp.dist
-		#if rank != 0 :
-		#	dist = com.gather(mp.dist[test], root=rank)
 
-		dist = com.reduce(mp.dist[test], op=MPI.MIN)
+	if True:
+		
+		print 'here 2'
+		#com.barrier()
+		dist = com.allreduce(mp.dist[test], op=MPI.MAX)
 		print "rank",rank,"dist", dist	,'test',test	
 		#if mp.dist[test] >= mp.dist[rank] + 1 :
 		if dist >= mp.dist[rank] + 1:
