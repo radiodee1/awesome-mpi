289b55fe8fae6511c11b9051f5366927f465805f
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-17 21:18:30 +0000

    first good looking results -- open spaces: good, walls: not good

--- dijkstra-mpi/array_setup.py
+++ dijkstra-mpi/array_setup.py
@@ -18,7 +18,7 @@ for i in range(100) :
 startx = 1
 starty = 0
 endx = 5
-endy = 0 #9
+endy = 9
 
 #enum
 OPEN = 1
@@ -37,13 +37,14 @@ for y in range (0 , 10):
 			main[ (y * 10) + x] = END
 			
 visited[(starty * 10) + startx] = VISITED
-#prev[(starty * 10) + startx] = 0
+
 			
 #non-random walls
+'''
 for i in range (0, 7) :
 	main[ (4 * 10) + i] = WALL
 	
 for i in range (4, 10) :
 	main[ (7 * 10) + i] = WALL
 	
-
+'''
--- dijkstra-mpi/node.py
+++ dijkstra-mpi/node.py
@@ -12,113 +12,96 @@ rank = com.Get_rank()
 dim = com.Get_size()
 
 
+directions = []
 
 def find() :
 
-	#print str(rank) +' ',
 	flag = 0
 	localflag = 0
 	ii = 0
-	
-	while flag == 0 :#or ii < 10 :
+	directions = []
+	while flag == 0 :
 	#if True:
-		#visited = com.alltoall(mp.visited)
-		#mp.visited[rank] = visited
-		print "ii",ii
-
+		print ii
 		mp.visited = com.allgather(mp.visited[rank])
 
-		#com.Barrier()
-		print "reduce", mp.visited, "rank", rank
-		
 		if mp.visited[rank] == mp.FREE and \
 				(mp.main[rank] != mp.WALL or mp.main[rank] == mp.START ): 
 			if get_y(rank) == get_y(rank + 1) and rank + 1 < dim and near_visited() :
 				set_must_check(rank + 1)
+				directions.append(rank + 1)
 			if get_y(rank) == get_y(rank - 1) and rank - 1 >= 0 and near_visited() :
 				set_must_check(rank - 1)
+				directions.append(rank - 1)
 			if rank + 10 < dim and near_visited() :
-				set_must_check(rank + 10) 
+				set_must_check(rank + 10)
+				directions.append(rank + 10) 
 			if rank - 10 >= 0 and near_visited() :
 				set_must_check(rank - 10)
+				directions.append(rank - 10)
 			if near_visited() and mp.visited[rank] == 0:
 				mp.visited[rank] = mp.VISITED
-				#mp.visited = com.allgather(mp.visited[rank])
-			#print 'here (didn\'t fire check)'
+				print directions
+				if len(directions) != 0:
+					mp.prev[rank] = min(directions) #test
+				directions = []
 		
 		#fix_prev()
-		mp.prev = com.allgather(mp.prev[rank])#, op=MPI.MAX)
-		print mp.prev, "---"
+		mp.prev = com.allgather(mp.prev[rank])
+		mp.dist = com.allgather(mp.dist[rank])
 		#com.barrier()
 		ii += 1
 		
 			
 		if mp.visited[rank] == mp.VISITED and mp.main[rank] == mp.END :
 			localflag = 1
-			print "end found!!" , rank
+			#print "end found!!" , rank
 		else:
 			localflag = 0
-			print 'not yet', rank
+			#print 'not yet', rank
 			
 		flag = com.allreduce(localflag, op=MPI.MAX)
-		if (rank == 0) :
-			print "end flag" , flag
-		print 'after flag reduce' 
-		#com.Barrier()
-		#dist = numpy.array(mp.dist, dtype=numpy.long)
-		#mp.dist = com.Bcast([dist, MPI.INT], root=rank)
-
+		#if (rank == 0) :
+		#	print "end flag" , flag
+		
 	
 	#print path backwards.
-	#com.barrier()
-	#mp.prev = com.allgather(mp.prev[rank])
-	#print mp.prev, rank
-	#found =com.reduce( mp.prev[(mp.endy * 10) + mp.endx], op=MPI.MAX)
+	
 	found = mp.prev[(mp.endy * 10) + mp.endx]
-	print "start of chain", found
 	if (rank == 0) or True:
 		i = 0
-		print "at start"
 		found = mp.prev[(mp.endy * 10) + mp.endx]
-		if found == rank:
-			print rank,"found start", found
+		if rank == 0 : #found == rank or True:
+			print rank,"found start", found,
 			while (found != 0) and i < 100 :
 				found = mp.prev[found]
-				#found = com.allreduce(mp.prev[found], op=MPI.MAX);
 				i += 1
-				print found," really found ",
-	
+				print "and ",found," really found ",
+			print mp.prev
 	
 def near_visited() :
 	
 	if get_y(rank) == get_y(rank + 1) and rank + 1 < dim :
 		if mp.visited[rank + 1] == mp.VISITED:
-			#mp.visited[rank] = mp.VISITED
 			return True
 	if get_y(rank) == get_y(rank - 1) and rank - 1 >= 0 :
 		if mp.visited[rank - 1] == mp.VISITED:
-			#mp.visited[rank] = mp.VISITED
 			return True
 	if rank + 10 < dim:
 		if mp.visited[rank + 10] == mp.VISITED:
-			#mp.visited[rank] = mp.VISITED
 			return True
 	if rank - 10 >= 0:
 		if mp.visited[rank -10] == mp.VISITED:
-			#mp.visited[rank] = mp.VISITED			
 			return True
 	if rank == get_rank(mp.startx, mp.starty) :
 		return True
 	return False
 		
-
-	
-def set_must_check(test):	
-	mp.prev[rank] = test
-	#prev = com.allgather(mp.prev)
-	print mp.prev
-	#mp.prev[test] = com.bcast(rank, root=rank)
-	print "set prev" , rank
+		
+def set_must_check(test):
+	#directions.append(test)
+		
+	#mp.prev[rank] = min(directions) #test
 	return 0		
 	
 def get_x(rank) :
