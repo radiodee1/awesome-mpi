9a760865e76c8ca168285a72253483de2c6b3e14
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-19 19:01:18 +0000

    script for large test

--- dijkstra-mpi/mpi_find.sh
+++ dijkstra-mpi/mpi_find.sh
@@ -1,3 +1,3 @@
-mpiexec.hydra -np 100 -f /mirror/awesome-mpi/machinefiles/machinefile-3x4 /mirror/awesome-mpi/dijkstra-mpi/find_path.py /mirror/awesome-mpi/dijkstra-mpi/wallfile-1.txt
+mpiexec.hydra -np 150 -f /mirror/awesome-mpi/machinefiles/machinefile-3x4 /mirror/awesome-mpi/dijkstra-mpi/find_path.py /mirror/awesome-mpi/dijkstra-mpi/wallfile-1.txt
 
 
