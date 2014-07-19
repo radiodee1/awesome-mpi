ddb4ad8c31e1bd9a9832745546d67c7c8ef8532d
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-19 17:48:36 +0000

    specify csv text file for walls

--- trunk/dijkstra-mpi/find_path.py
+++ trunk/dijkstra-mpi/find_path.py
@@ -4,7 +4,7 @@ from mpi4py import MPI
 import array_setup as mp
 #import numpy
 #import math
-#import sys
+import sys
 import node
 
 com = MPI.COMM_WORLD
@@ -13,7 +13,7 @@ dim = com.Get_size()
 
 if (dim != mp.height * mp.width and rank == 0) :
 	print 'size must be', mp.height*mp.width,', (' ,mp.width, 'x',mp.height, ')!!'
-#if dim != mp.height * mp.width :
-#	sys.exit()
+if dim != mp.height * mp.width :
+	sys.exit()
 	
 node.find();
