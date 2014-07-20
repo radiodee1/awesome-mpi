21f9f64fd56dd2520ee14de4f3fee9501ee2adf4
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-20 18:32:13 +0000

    add echo line to launcher shell scripts.

--- trunk/dijkstra-mpi/mpi_find_20x20.sh
+++ trunk/dijkstra-mpi/mpi_find_20x20.sh
@@ -1,3 +1,5 @@
+echo 'if this file does not work edit the array_setup.py file '
+
 mpiexec.hydra -np 400 -f /mirror/awesome-mpi/machinefiles/machinefile-3x4 /mirror/awesome-mpi/dijkstra-mpi/find_path.py /mirror/awesome-mpi/dijkstra-mpi/wallfile-20x20.txt
 
 
