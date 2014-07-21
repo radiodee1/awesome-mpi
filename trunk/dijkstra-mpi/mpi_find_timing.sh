6c696fbab17b09b65b9fef3ce7530319ed13ee0a
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-21 20:40:46 +0000

    script to print timing

--- trunk/dijkstra-mpi/mpi_find_timing.sh
+++ trunk/dijkstra-mpi/mpi_find_timing.sh
@@ -3,6 +3,12 @@ echo 'this may take a while'
 
 mpiexec -n 100 -f ../machinefiles/machinefile-3x4 ./trace_my_trace.py find ./find_path.py > cluster_3x4_my_trace_date_find.txt
 
+echo 'one done'
+
 mpiexec -n 100 -f ../machinefiles/machinefile-3x4 ./trace_my_trace.py fix_prev ./find_path.py > cluster_3x4_my_trace_date_fix_prev.txt
 
+echo 'two done'
+
 mpiexec -n 100 -f ../machinefiles/machinefile-3x4 ./trace_my_trace.py fix_dist ./find_path.py > cluster_3x4_my_trace_date_fix_dist.txt
+
+echo 'three done'
