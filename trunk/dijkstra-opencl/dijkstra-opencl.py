bec50ee444d779a0b0c6e4619e395329e7937556
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-25 13:27:48 +0000

    add files

--- trunk/dijkstra-opencl/dijkstra-opencl.py
+++ trunk/dijkstra-opencl/dijkstra-opencl.py
@@ -14,10 +14,17 @@ import sys
 
 
 class CL(object):
+
+
     def __init__(self, size=10):
         self.size = size
         self.ctx = cl.create_some_context()
         self.queue = cl.CommandQueue(self.ctx)
+
+	self.FREE = 0
+	self.WALL = 1
+	self.UNDEFINED = -1
+	self.VISITED = 1
  
     def load_program(self):
         fstr="""
@@ -27,7 +34,7 @@ class CL(object):
  			return 2*(a + b);
             
         }
-         __kernel void part1(__global float* a, __global float* b, __global float* c)
+         __kernel void find(__global float* maze, __global float* visited, __global float* dist, __global float* prev)
         {
             unsigned int i = get_global_id(0);
  
@@ -37,35 +44,47 @@ class CL(object):
         self.program = cl.Program(self.ctx, fstr).build()
  
     def popCorn(self):
+    	#
         mf = cl.mem_flags
  
         self.a = numpy.array(range(self.size), dtype=numpy.float32)
         self.b = numpy.array(range(self.size), dtype=numpy.float32)
         
-        self.maze = numpy.array(range(self.size), dtype=numpy.float32)
-        self.visited = numpy.array(range(self.size), dtype=numpy.float32)
-        self.dist = numpy.array(range(self.size), dtype=numpy.float32)
-        self.prev = numpy.array(range(self.size), dtype=numpy.float32)
+        self.maze = numpy.array([self.FREE] * self.size, dtype=numpy.float32)
+        self.visited = numpy.array(([self.FREE] * self.size), dtype=numpy.float32)
+        self.dist = numpy.array(([self.UNDEFINED] * self.size), dtype=numpy.float32)
+        self.prev = numpy.array(([self.UNDEFINED] * self.size), dtype=numpy.float32)
  
         self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
                                hostbuf=self.a)
                                
-        self.maze_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
+        self.maze_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR,
                                hostbuf=self.maze)   
                                                    
         self.b_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
                                hostbuf=self.b)
         self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.b.nbytes)
         
-        self.visited_buf = cl.Buffer(self.ctx, mf.READ_WRITE, self.maze.nbytes)
-        self.dist_buf = cl.Buffer(self.ctx, mf.READ_WRITE, self.maze.nbytes)
-        self.prev_buf = cl.Buffer(self.ctx, mf.READ_WRITE, self.maze.nbytes)
+        self.visited_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
+        						hostbuf=self.visited)
+        self.dist_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
+        						hostbuf=self.dist)
+        self.prev_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
+        						hostbuf=self.prev)
         
  
     def execute(self):
-        self.program.part1(self.queue, self.a.shape, None, self.a_buf, self.b_buf, self.dest_buf)
-        c = numpy.empty_like(self.a)
-        cl.enqueue_read_buffer(self.queue, self.dest_buf, c).wait()
+        self.program.find(self.queue, self.a.shape, None, 
+        		self.maze_buf, self.visited_buf, self.dist_buf, self.prev_buf)
+        visited = numpy.empty_like(self.a)
+        dist = numpy.empty_like(self.a)
+        prev = numpy.empty_like(self.a)
+        
+        #cl.enqueue_read_buffer(self.queue, self.dest_buf, c).wait()
+        
+        cl.enqueue_read_buffer(self.queue, self.visited_buf, visited).wait()
+        cl.enqueue_read_buffer(self.queue, self.dist_buf, dist).wait()
+        cl.enqueue_read_buffer(self.queue, self.prev_buf, prev).wait()        
         #print ( "a", self.a)
         #print ( "b", self.b)
         print ( "c", c )
