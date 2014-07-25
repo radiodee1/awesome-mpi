99327a0f4532f71a6ec269bb1b2db31b8ff4265f
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-25 18:29:31 +0000

    add files -- start on kernel

--- trunk/dijkstra-opencl/dijkstra-opencl.py
+++ trunk/dijkstra-opencl/dijkstra-opencl.py
@@ -8,9 +8,6 @@ import fileinput
 from PIL import Image
 import pygame as pg
 
-#from OpenGL.GLUT import *
-#from OpenGL.GLU import *
-#from OpenGL.GL import *
 import sys
 
 import array_setup as mz
@@ -18,43 +15,39 @@ import array_setup as mz
 class CL(object):
 
 
-    def __init__(self, width=10, height=10):
-    	self.width = width
-    	self.height = height
-        self.size = width*height
-        self.ctx = cl.create_some_context()
-        self.queue = cl.CommandQueue(self.ctx)
+	def __init__(self):
+		self.width = mz.width
+		self.height = mz.height
+		self.size = mz.width * mz.height
+		self.prev = []
+		
+		self.ctx = cl.create_some_context()
+		self.queue = cl.CommandQueue(self.ctx)
 
-	'''
-	self.FREE = 0
-	self.WALL = 1
-	self.UNDEFINED = -1
-	self.VISITED = 1
- 	'''
- 
-    def load_program(self):
-    	fstr = ''
-    	for line in fileinput.input('find.cl'):
-    		fstr += line
-    
-        self.program = cl.Program(self.ctx, fstr).build()
- 
-    def set_buffers(self):
+
+	def load_kernel(self):
+		fstr = ''
+		for line in fileinput.input('find.cl'):
+			fstr += line
+
+		self.program = cl.Program(self.ctx, fstr).build()
+
+	def set_buffers(self):
 		#
 		mf = cl.mem_flags
 
-		
+	
 		self.maze = numpy.array(mz.maze, dtype=numpy.float32)
 		self.visited = numpy.array(([mz.FREE] * self.size), dtype=numpy.float32)
 		self.dist = numpy.array(([mz.UNDEFINED] * self.size), dtype=numpy.float32)
 		self.prev = numpy.array(([mz.UNDEFINED] * self.size), dtype=numpy.float32)
 
 		self.dimension = numpy.array(([self.width, self.height]), dtype=numpy.float32)
-		               
+			           
 		self.maze_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR,
-				               hostbuf=self.maze)   
-				                                   
-		
+					           hostbuf=self.maze)   
+					                               
+	
 		self.visited_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
 								hostbuf=self.visited)
 		self.dist_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
@@ -63,96 +56,118 @@ class CL(object):
 								hostbuf=self.prev)
 		self.dimension_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, 
 								hostbuf=self.dimension)
- 
-    def execute(self):
-        self.program.find(self.queue, self.maze.shape, None, 
-        		self.maze_buf, self.visited_buf, self.dist_buf, self.prev_buf, 
-        		self.dimension_buf)
-        		
-        visited = numpy.empty_like(self.maze)
-        dist = numpy.empty_like(self.maze)
-        prev = numpy.empty_like(self.maze)
-        
 
-        
-        cl.enqueue_read_buffer(self.queue, self.visited_buf, visited).wait()
-        cl.enqueue_read_buffer(self.queue, self.dist_buf, dist).wait()
-        cl.enqueue_read_buffer(self.queue, self.prev_buf, prev).wait()        
-        #print ( "a", self.a)
-        #print ( "b", self.b)
-        print prev
-        print mz.maze
- 
-def add(s=10) :
-    starttime = time.clock()
-    matrixd = CL(10,10)
-    matrixd.load_program()
-    matrixd.set_buffers()
-    matrixd.execute()
-    endtime = time.clock()
-    print s, endtime - starttime 
+	def execute(self):
+		self.program.find(self.queue, self.maze.shape, None, 
+				self.maze_buf, self.visited_buf, self.dist_buf, self.prev_buf, 
+				self.dimension_buf)
+				
+		visited = numpy.empty_like(self.maze)
+		dist = numpy.empty_like(self.maze)
+		prev = numpy.empty_like(self.maze)
+		'''
+			at some point may remove 'wait' on visited_buf and dist_buf!!
+		'''
+		cl.enqueue_read_buffer(self.queue, self.visited_buf, visited).wait()
+		cl.enqueue_read_buffer(self.queue, self.dist_buf, dist).wait()
+		cl.enqueue_read_buffer(self.queue, self.prev_buf, prev).wait()        
+		
+		self.prev = prev
+		#print prev
+		#print mz.maze
+		
+	def get_prev(self):
+		return self.prev
 
-def show_png():
-	surface = pg.image.load('map.png')
-	string = pg.image.tostring(surface, 'RGB')
-	#print string
+	def get_width(self):
+		return self.width
 	
-	white = (64, 64, 64)
-	w = 640
-	h = 480
-	screen = pg.display.set_mode((w, h))
-	screen.fill((white))
-	running = 1
+	def get_height(self):
+		return self.height
 
-	while running:
+class Interface(object) :
+
+	def __init__(self):
+		self.mapname = 'map.png'	
+		
+	def show_png(self):
+		surface = pg.image.load(self.mapname)
+		string = pg.image.tostring(surface, 'RGB')
+		#print string
 	
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				sys.exit()
+		white = (64, 64, 64)
+		w = 640
+		h = 480
+		screen = pg.display.set_mode((w, h))
 		screen.fill((white))
-		screen.blit(surface,(0,-10))
-		pg.display.flip()
+		running = 1
+
+		while running:
+	
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
+					sys.exit()
+			screen.fill((white))
+			screen.blit(surface,(0,0))
+			pg.display.flip()
 
 
-def show_maze():
-	if True:	
-		print
-		for x in range (0,mz.width + 2):
-			if x < mz.width + 1:
-				print '#',
-			else:
-				print '#'
-
-		for y in range (0 , mz.height):
-			print '#',
-			for x in range (0, mz.width):
-				
-				if mz.maze[ (y * mz.width) + x] == mz.FREE :
-					print ' ',
-				if mz.maze[ (y * mz.width) + x] == mz.START :
-					print 'S',
-				if mz.maze[ (y * mz.width) + x] == mz.END :
-					print 'X',
-				if mz.maze[ (y * mz.width) + x] == mz.WALL :
-					print '#',
-				if mz.maze[ (y * mz.width) + x] == mz.PATH :
-					print 'O',
-				
-				
-			print '#',
+	def show_maze(self, maze = [], width = 10, height = 10):
+		
+		if len(maze) != height * width:
+			#
+			print 'bad height * width'
+		
+		
+		if True:	
 			print
+			for x in range (0,mz.width + 2):
+				if x < mz.width + 1:
+					print '#',
+				else:
+					print '#'
 
-		for x in range (0,mz.width + 2):
-			print '#',
-		print
-		print
+			for y in range (0 , mz.height):
+				print '#',
+				for x in range (0, mz.width):
+				
+					if maze[ (y * mz.width) + x] == mz.FREE :
+						print ' ',
+					if maze[ (y * mz.width) + x] == mz.START :
+						print 'S',
+					if maze[ (y * mz.width) + x] == mz.END :
+						print 'X',
+					if maze[ (y * mz.width) + x] == mz.WALL :
+						print '#',
+					if maze[ (y * mz.width) + x] == mz.PATH :
+						print 'O',
+				
+				
+				print '#',
+				print
+
+			for x in range (0,mz.width + 2):
+				print '#',
+			print
+			print
 		
 
 
 if __name__ == '__main__': 
-	add()
-	show_maze()
-	show_png()
+	starttime = time.clock()
+	matrixd = CL()
+	matrixd.load_kernel()
+	matrixd.set_buffers()
+	matrixd.execute()
+	a = matrixd.get_prev()
+	print a
+	endtime = time.clock()
+	print  endtime - starttime 
+
+	i = Interface()
+	i.show_maze(mz.maze, mz.width, mz.height)
+	i.show_maze(a , matrixd.get_width(), matrixd.get_height())
+	i.show_png()
 	
 	
 	
--- trunk/dijkstra-opencl/find.cl
+++ trunk/dijkstra-opencl/find.cl
@@ -1,9 +1,14 @@
-float add(float a, float b)
-        {
-            
- 			return 2*(a + b);
-            
+		int get_x(int width,  int ii) {
+ 			return ii - (width * (ii / width));   
         }
+        
+        int get_y(int width, int ii) {
+        	return ii / width;
+        }
+        int get_index(int width, int x, int y) {
+        	return (y * width) + x;
+        }
+        
          __kernel void find(
          		__global float* maze, 
          		__global float* visited, 
@@ -12,11 +17,52 @@ float add(float a, float b)
          		__global float* dimension)
          		
         {
+        	int FREE = 0;
+        	int OPEN = 1;
+        	int WALL = 2;
+        	int START = 3;
+        	int END = 4;
+        	int PATH = 5;
+
+			int VISITED = 1;
+			int UNDEFINED = -1;
+			
             unsigned int ii = get_global_id(0);
             
  			unsigned int width = dimension[0];
  			unsigned int height = dimension[1];
+ 			unsigned int dim = width * height;
  			
-           //c[i] = add (a[i] , b[i]);
-           prev[ii] = maze[ii];
+ 			unsigned int flag = 0;
+ 			unsigned int localflag = 0;
+ 			unsigned int i = 0;
+       		
+       		while (flag == 0 && i < dim) {
+       		
+		   		if (visited[ii] ==  FREE && main[ii] !=  WALL) {
+						
+					if (get_y(width,ii) == get_y(width,ii + 1)  && ii + 1 < dim  && near_visited()) {
+						must_check(ii + 1,  RIGHT);
+					}
+					if (get_y(width, ii) == get_y(width, ii - 1)  && ii - 1 >= 0  && near_visited()) {
+						must_check(ii - 1,  LEFT);
+					}
+					if (ii +  width < dim  && near_visited()) {
+						must_check(ii +  width,  DOWN);
+					}
+					if (ii -  width >= 0  && near_visited()) {
+						must_check(ii -  width,  UP);
+					}
+					if  (main[ii] ==  START) {
+						must_check(ii,  CENTER);
+					}
+
+					if (near_visited()) {
+						 visited[ii] =  VISITED;
+					}
+				}
+       		}
+           
+           prev[ii] =  maze[ii];
+           
         }
