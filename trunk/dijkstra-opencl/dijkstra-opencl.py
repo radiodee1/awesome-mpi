a72bde3f423c15b29b7527086b8109c0d0a174fb
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-07-27 20:21:04 +0000

    info functions give clue to sizes for mazes -- 28x28 works

--- trunk/dijkstra-opencl/dijkstra-opencl.py
+++ trunk/dijkstra-opencl/dijkstra-opencl.py
@@ -30,9 +30,9 @@ class CL(object):
 		print cl.kernel_work_group_info.WORK_GROUP_SIZE, 'work group size'
 		print cl.kernel_work_group_info.PREFERRED_WORK_GROUP_SIZE_MULTIPLE , 'preferred size'
 		
-		print mz.width * mz.height * 5 , 'calculated use'# 4.5 is num of full buffers!
+		print mz.width * mz.height * 5 , 'calculated use'# 5 is num of full buffers!
 		
-		print 'reccomended' , math.sqrt(cl.device_info.MAX_WORK_GROUP_SIZE/5)
+		print 'estimated:' , int(math.sqrt(cl.device_info.MAX_WORK_GROUP_SIZE / 5))
 		
 		self.ctx = cl.create_some_context()
 		self.queue = cl.CommandQueue(self.ctx,
