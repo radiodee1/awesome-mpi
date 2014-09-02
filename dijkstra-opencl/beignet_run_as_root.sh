08cbe06502b5c75175dcd8fe4cb12c7890729d68
david.c.liebman@gmail.com <david.c.liebman@gmail.com@1af93e2c-cbf6-675e-65b0-dff216a3ac3d>
2014-09-02 16:13:04 +0000

    beignet requirements

--- dijkstra-opencl/beignet_run_as_root.sh
+++ dijkstra-opencl/beignet_run_as_root.sh
@@ -2,3 +2,5 @@ echo 0 > /sys/module/i915/parameters/enable_cmd_parser
 
 # As of 9/2/2014 this is needed to allow Beignet to work.
 # It's not needed otherwise.
+#
+# Remains in effect until system restart.
